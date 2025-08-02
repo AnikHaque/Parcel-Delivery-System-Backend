8adf1389f1da1b118cccdc40eb2d2972e47891b8
AnikHaque <ekramul15-811@diu.edu.bd>
2025-08-02 23:07:05 +0600

    all done

--- src/app.ts
+++ src/app.ts
@@ -21,7 +21,7 @@ app.use(cors())
 app.use('/api/v1',router)
 
 app.get('/', (req: Request, res: Response) => {
-   res.send("Welcome to Parcel Delivery System")
+   res.send("Welcome to Parcel Delivery System Backend")
 })
 
 
